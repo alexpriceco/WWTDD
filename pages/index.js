102a7a68ff9f7b6320bb4cd5ed8a2ce5bff6855c
Alex Price <alex@alexprice.co>
2018-08-14 17:12:00 -0500

    Add zigzag, started first section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head'
 import styled, { injectGlobal } from 'styled-components'
 
+import ZigZag from '../assets/zigzag.js'
+
 const Header = styled.div`
   width: 100vw;
   height: 80vh;
   background: #FBF9F3;
   padding: 100px;
   box-sizing: border-box;
+  position: relative;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
 
   h1 {
     color: #030C24;
@@ -14,7 +19,30 @@ const Header = styled.div`
   }
 `
 
+const ZigZagContainer = styled.div`
+  width: 100vw;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  svg {
+    width: 100%;
+    position: absolute;
+    bottom: -30px;
+    left: 0;
+  }
+`
+
+const Currently = styled.div`
+  width: 100vw;
+  padding: 100px;
+  background: #0E204E;
+  color: #FBF9F4;
+`
+
 injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Work+Sans:400,500');
+
   body {
     margin: 0;
     padding: 0;
@@ -38,6 +66,12 @@ export default () => (
       <h1>
         Wait, what's the designer doing?
       </h1>
+      <ZigZagContainer>
+        <ZigZag />
+      </ZigZagContainer>
     </Header>
+    <Currently>
+      stuff
+    </Currently>
   </div>
 )
